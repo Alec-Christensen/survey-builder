909887295504c945a5fc429b0e3b4b6a5e4ed496
Alec Christensen <alec_christensen@hotmail.com>
2026-05-18 13:00:57 +0200

    chore: add global css variables and navbar component

---
+++ SurveyBuilder/survey-builder-client/src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+interface NavbarProps {
+  primaryAction?: { label: string; onClick: () => void }
+  secondaryAction?: { label: string; onClick: () => void }
+}
+
+export default function Navbar({ primaryAction, secondaryAction }: NavbarProps) {
+  return (
+    <nav className="navbar">
+      <Link to="/dashboard" className="navbar-brand">SurveyBuilder</Link>
+      <div className="navbar-actions">
+        {secondaryAction && (
+          <button className="btn btn-secondary btn-sm" onClick={secondaryAction.onClick}>
+            {secondaryAction.label}
+          </button>
+        )}
+        {primaryAction && (
+          <button className="btn btn-primary btn-sm" onClick={primaryAction.onClick}>
+            {primaryAction.label}
+          </button>
+        )}
+      </div>
+    </nav>
+  )
+}
